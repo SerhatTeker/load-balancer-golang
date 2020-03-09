507d4bb65b1dd2ba16f1fcc805a1440d033a1f3b
Serhat Teker <serhat.teker@gmail.com>
2020-03-09 15:47:16 +0300

    Add initial project

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+var (
+	counter int
+
+	//TODO configurable
+	listenAddr = "localhost:8080"
+
+	//TODO configurable
+	server = []string{
+		"localhost:5000",
+		"localhost:5001",
+		"localhost:5002",
+	}
+)
+
+func main() {
+	listener, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		log.Fatalf("failed to listen: %s:, err")
+	}
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("failed to accept connection: %s", err)
+		}
+
+		backend := chooseBackend()
+		fmt.Println("counter=%d backend=%s\n", counter, backend)
+		go func() {
+			err := proxy(backend, conn)
+			if err != nil {
+				log.Printf("WARNING: proxying failed: %v", err)
+			}
+		}()
+	}
+}
+
+func proxy(backend string, c net.Conn) error {
+	bc, err := net.Dial("tcp", backend)
+	if err != nil {
+		return fmt.Errorf("failed to connect to backend %s: %v", backend, err)
+	}
+
+	//c -> bc
+	go io.Copy(bc, bc)
+
+	//bc -> c
+	go io.Copy(c, bc)
+
+	return nil
+}
+
+func chooseBackend() string {
+	s := server[counter%len(server)]
+	counter++
+	return s
+}
